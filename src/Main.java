3bea2db44ab9f9458306a29a117063d25e0c6667
ANDRE <klai1@mail.ru>
2022-07-28 21:33:50 +0400

    Все задания выполнил

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int dog=1;
         byte appl=3;
         short cat=4;
-        long snake=3l;
+        long snake=3L;
         float bus=3.5f;
         double suggart=3.7;
         boolean dogIsAdult=dog>10;
