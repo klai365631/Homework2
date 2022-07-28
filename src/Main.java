117b128dacf861f6081cabc4a0cea430f33296b1
ANDRE <klai1@mail.ru>
2022-07-28 17:13:41 +0400

    Удалил запись

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("пишем дз!!!");
         System.out.println("делаем коммит");
-        System.out.println("Переходим в гитхаб");
+
 
     }
 }
