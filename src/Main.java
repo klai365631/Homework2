db9ae05e51eb693e7c13256a63e857cb0481db0c
ANDRE <klai1@mail.ru>
2022-07-28 17:41:33 +0400

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
         int dog=1;
         byte appl=3;
         short cat=4;
-        long snake=3;
+        long snake=3l;
         float bus=3.5f;
         double suggart=3.7;
         boolean dogIsAdult=dog>10;
         char bar=35;
         System.out.println(dogIsAdult);
+
+
 
 
 
