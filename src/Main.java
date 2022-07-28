2f03ea45c9637a1eaa3eced64e22a0f9f4abd999
ANDRE <klai1@mail.ru>
2022-07-28 16:57:06 +0400

    Выполнил дз номер 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("пишем дз!!!");
+        System.out.println("делаем коммит");
+        System.out.println("Переходим в гитхаб");
+
     }
 }
